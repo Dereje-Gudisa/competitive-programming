dcf900c03a259e742a3fe1c151a522e5ac742f3f
Dereje-Gudisa <160274918+Dereje-Gudisa@users.noreply.github.com>
2026-03-11 21:58:03 +0300

    Time: 0 ms (100.00%) | Memory: 53.6 MB (58.14%) - LeetSync

---
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var bitwiseComplement = function(n) {
+  if (n === 0) return 1;
+
+    let mask = 1;
+
+    while (mask <= n) {
+        mask <<= 1;
+    }
+
+    return (mask - 1) ^ n;
+};
