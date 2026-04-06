f26afaa72004383b53ee88cabe41123becb8c905
Dereje Gudisa <160274918+Dereje-Gudisa@users.noreply.github.com>
2026-04-06 21:23:35 +0300

    Time: 0 ms (100.00%) | Memory: 54.9 MB (23.95%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    // Copy nums2 into the empty space of nums1
+    for (let i = 0; i < n; i++) {
+        nums1[m + i] = nums2[i];
+    }
+    // Sort the entire array
+    nums1.sort((a, b) => a - b);
+    
+};
