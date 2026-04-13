2f5f1b2bf0b8a24035f4e3a7b77c2c18fb2f9429
Dereje Gudisa <160274918+Dereje-Gudisa@users.noreply.github.com>
2026-04-13 23:04:50 +0300

    Time: 1 ms (33.33%) | Memory: 53.6 MB (64.58%) - LeetSync

---
+++ 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @param {number} start
+ * @return {number}
+ */
+var getMinDistance = function(nums, target, start) {
+    let minDistance = Infinity;
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            const distance = Math.abs(i - start);
+            minDistance = Math.min(minDistance, distance);
+        }
+    }
+    
+    return minDistance;
+    
+};
