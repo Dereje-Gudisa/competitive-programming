96b3e06456a55084acb728b45cca4795ab36cd2e
Dereje Gudisa <160274918+Dereje-Gudisa@users.noreply.github.com>
2026-05-23 18:22:58 +0300

    Time: 0 ms (100.00%) | Memory: 53.4 MB (65.44%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var check = function(nums) {
+    let dropCount = 0;
+    const n = nums.length;
+
+    for (let i = 0; i < n; i++) {
+        if (nums[i] > nums[(i + 1) % n]) {
+            dropCount++;
+        }
+    }
+
+    return dropCount <= 1;
+    
+};
