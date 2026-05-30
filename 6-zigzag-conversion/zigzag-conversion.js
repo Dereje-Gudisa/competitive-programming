b470920089af0c7dff49b9ff1e765c541ae07d04
Dereje Gudisa <160274918+Dereje-Gudisa@users.noreply.github.com>
2026-05-30 22:29:21 +0300

    Time: 2 ms (97.03%) | Memory: 57.5 MB (79.64%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    if (numRows === 1 || numRows >= s.length) return s;
+
+    const rows = new Array(numRows).fill("");
+    let row = 0;
+    let goingDown = false;
+
+    for (let i = 0; i < s.length; i++) {
+        rows[row] += s[i];
+
+        if (row === 0 || row === numRows - 1) {
+            goingDown = !goingDown;
+        }
+
+        row += goingDown ? 1 : -1;
+    }
+
+    return rows.join("");
+    
+};
